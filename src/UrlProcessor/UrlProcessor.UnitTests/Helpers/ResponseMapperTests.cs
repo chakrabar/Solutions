993eb2978c2c63f03c6808c255c9e2d9fdb81e5d
Arghya C <chakrabar@users.noreply.github.com>
2018-12-01 19:50:46 +0530

    Say hello to the tests

---
+++ src/UrlProcessor/UrlProcessor.UnitTests/Helpers/ResponseMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrlProcessor.Helpers;
+
+namespace UrlProcessor.UnitTests.Helpers
+{
+    [TestClass]
+    public class ResponseMapperTests
+    {
+        [TestMethod]
+        public void Should_map_status_correctly()
+        {
+            var result = ResponseMapper.GetQueuingResponse(1, SharedModels.QueuingStatus.INPROGRESS);
+
+            Assert.AreEqual(result.RequestStatus, ViewModels.RequestStatus.Successful);
+        }
+
+        [TestMethod]
+        public void Should_map_error_status()
+        {
+            var result = ResponseMapper.GetQueuingResponse(1, SharedModels.QueuingStatus.FAILED);
+
+            Assert.AreEqual(result.RequestStatus, ViewModels.RequestStatus.Failed);
+        }
+    }
+}
