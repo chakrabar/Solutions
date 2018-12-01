7cc2b0681b7b5f0f34625ebd14817126cb39c939
Arghya C <chakrabar@users.noreply.github.com>
2018-12-01 20:58:09 +0530

    App tests are coming in

---
+++ src/UrlProcessor/Core.UnitTests/ResourceProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using AppContracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Core.UnitTests
+{
+    [TestClass]
+    public class ResourceProcessorTests
+    {
+        Mock<IProcessQueue> _queueMock;
+        Mock<IUrlBatchProcessor> _batchProcessorMock;
+        ResourceProcessor _app;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _queueMock = new Mock<IProcessQueue>();
+            _batchProcessorMock = new Mock<IUrlBatchProcessor>();
+            _app = new ResourceProcessor(_queueMock.Object, _batchProcessorMock.Object);
+        }
+    }
+}
