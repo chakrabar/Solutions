91420f5d83d778f9855883f00a9a41aaa8c1f765
chakrabar <chakrabar@live.com>
2018-12-13 00:03:50 +0530

    Hosting ASP.NET Core as Windows service

--- src/LocalLogService/LocalLogService/Program.cs
+++ src/LocalLogService/LocalLogService/Program.cs
@@ -1,17 +1,64 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace LocalLogService
 {
     public class Program
     {
+        public static void Main2(string[] args)
+        {
+            CreateWebHostBuilder2(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder2(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateWebHostBuilder(
+                args
+                .Where(arg => arg != "--console")
+                .ToArray());
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                // To run the app without the CustomWebHostService change the
+                // next line to host.RunAsService();
+                // host.RunAsCustomService(); //commented - no custom web service
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddEventLog();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                // Configure the app here.
+            })
                 .UseStartup<Startup>();
     }
 }
