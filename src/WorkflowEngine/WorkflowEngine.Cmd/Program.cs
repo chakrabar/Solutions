12e8c5889ee79828990540431cc188a27634bbe4
chakrabar <chakrabar@live.com>
2019-01-05 00:24:50 +0530

    Experiments for workflow engine container

---
+++ src/WorkflowEngine/WorkflowEngine.Cmd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WorkflowEngine.Cmd.Workflows;
+using WorkflowEngine.Utilities;
+
+namespace WorkflowEngine.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var activity = new AddWorkflow();
+
+
+            //var xaml = activity.ToXamlString();
+            //Console.WriteLine(xaml);
+
+            //activity.WriteToXamlFile($@"C:\ArghyaC\repos\Solutions\src\WorkflowEngine\TestData\AddWorkflow_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.xaml");
+
+            var activity = SampleActivityBuilder.GetActivityBuilder();
+
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+
+            var xaml2 = activity.ToXamlString2();
+            Console.WriteLine(xaml2);
+
+            activity.WriteToXamlFile2($@"C:\ArghyaC\repos\Solutions\src\WorkflowEngine\TestData\AddWorkflow_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.xaml");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/WorkflowEngine/WorkflowEngine.Cmd/Workflows/AddWorkflow.cs
@@ -0,0 +1,18 @@
+﻿using System.Activities;
+
+namespace WorkflowEngine.Cmd.Workflows
+{
+    public class AddWorkflow : CodeActivity<int>
+    {
+        [RequiredArgument]
+        public InArgument<int> LeftVal { get; set; }
+
+        [RequiredArgument]
+        public InArgument<int> RightVal { get; set; }
+
+        protected override int Execute(CodeActivityContext context)
+        {
+            return LeftVal.Get(context) + RightVal.Get(context);
+        }
+    }
+}
