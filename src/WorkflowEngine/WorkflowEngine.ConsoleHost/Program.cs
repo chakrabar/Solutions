06d5f180ceb667d24fd5d70edfa104d13d8496b6
chakrabar <chakrabar@live.com>
2019-01-06 13:53:12 +0530

    Basic code to run a workflow, go idle & resume. Also handles abort & exceptions.

---
+++ src/WorkflowEngine/WorkflowEngine.ConsoleHost/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WorkflowEngine.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();            
+        }
+
+        IDictionary<string, object> Host(Activity activity, IDictionary<string, object> inputs)
+        {
+            IDictionary<string, object> outputs = new Dictionary<string, object>();
+            AutoResetEvent syncEvent = new AutoResetEvent(false); //initially closed
+            AutoResetEvent idleEvent = new AutoResetEvent(false); //initially closed
+
+            WorkflowApplication wfApp = new WorkflowApplication(activity, inputs)
+            {
+                Completed = delegate (WorkflowApplicationCompletedEventArgs e)
+                {
+                    outputs = e.Outputs;
+                    syncEvent.Set();
+                },
+                Aborted = delegate (WorkflowApplicationAbortedEventArgs e)
+                {
+                    Console.WriteLine(e.Reason);
+                    syncEvent.Set();
+                },
+                OnUnhandledException = delegate (WorkflowApplicationUnhandledExceptionEventArgs e)
+                {
+                    Console.WriteLine(e.UnhandledException.ToString());
+                    return UnhandledExceptionAction.Terminate;
+                },
+                Idle = delegate (WorkflowApplicationIdleEventArgs e)
+                {
+                    idleEvent.Set(); //when goes idle, let the process continue
+                }
+        };
+
+            wfApp.Run();
+
+            //syncEvent.WaitOne(); //wait for the application to COMPLETE / ABORT  ///// GO IDLE
+
+            // Loop until the workflow for COMPLETE / ABORT / GO IDLE
+            WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
+            while (WaitHandle.WaitAny(handles) != 0) //this is blocking & waiting, we need to fix this with persist & resume
+            {
+                // Gather the user input and resume the bookmark.
+                bool validEntry = false;
+                while (!validEntry)
+                {
+                    int Guess;
+                    if (!Int32.TryParse(Console.ReadLine(), out Guess)) //this is custom logic, use a Func<data, bool instead>
+                    {
+                        Console.WriteLine("Please enter an integer.");
+                    }
+                    else
+                    {
+                        validEntry = true;
+                        wfApp.ResumeBookmark("EnterGuess", Guess);
+                    }
+                }
+            }
+
+            // (assuming the workflow completed) return results
+            return outputs;
+        }
+    }
+}
---
+++ src/WorkflowEngine/WorkflowEngine.Core/ApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+
+namespace WorkflowEngine.Core
+{
+    public class ApplicationFactory
+    {
+        public void Get(Activity activity)
+        {
+            WorkflowApplication wfApp = new WorkflowApplication(activity);
+
+            Console.WriteLine("Id: {0}", wfApp.Id);
+        }
+    }
+}
