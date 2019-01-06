13651e395cd1d332e6b54b4a974399262677933e
chakrabar <chakrabar@live.com>
2019-01-07 01:17:40 +0530

    Updating trace listener

--- src/WorkflowContainer/WorkflowContainer.Activities/RequestHumanInput.cs
+++ src/WorkflowContainer/WorkflowContainer.Activities/RequestHumanInput.cs
@@ -19,7 +19,7 @@ namespace WorkflowContainer.Activities
 
         public void OnResumeBookmark(NativeActivityContext context, Bookmark bookmark, object obj)
         {
-            Trace.TraceInformation("Re-starting bookmark : " + bookmark.Name);
+            Trace.TraceInformation($"From : {nameof(RequestHumanInput<T>)}. Re-starting bookmark : {bookmark.Name}");
             T output = default(T);
             if (obj is T)
             {
@@ -31,25 +31,27 @@ namespace WorkflowContainer.Activities
             }
             catch (InvalidCastException e)
             {
-                Trace.TraceError($"Error casting data. Exception: {e.Message}");
+                Trace.TraceError($"From : {nameof(RequestHumanInput<T>)}. Error casting data. Exception: {e.Message}");
                 throw;
             }
 
-            Trace.TraceInformation("Bookmark resume data : " + output.ToString());
+            Trace.TraceInformation($"From : {nameof(RequestHumanInput<T>)}. Bookmark resume data : {output.ToString()}");
             // When the Bookmark is resumed, assign its value to the Result argument
             Result.Set(context, output);
+            Trace.Flush();
         }
 
         protected override void Execute(NativeActivityContext context)
         {
             // Send request to human
-            Trace.TraceInformation("Request message : " + RequestMessage.Get(context));
+            Trace.TraceInformation($"From : {nameof(RequestHumanInput<T>)}. Request message : {RequestMessage.Get(context)}");
 
-            Trace.TraceInformation("Creating bookmark : " + BookmarkName.Get(context));
+            Trace.TraceInformation($"From : {nameof(RequestHumanInput<T>)}. Creating bookmark : {BookmarkName.Get(context)}");
 
             // Create a Bookmark and wait for it to be resumed.  
             context.CreateBookmark(BookmarkName.Get(context),
                 new BookmarkCallback(OnResumeBookmark));
+            Trace.Flush();
         }
     }
 }
