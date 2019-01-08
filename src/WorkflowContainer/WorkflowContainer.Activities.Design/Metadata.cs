f7cd7d5e31c358fafdb153253c1f844ae8629060
chakrabar <chakrabar@live.com>
2019-01-08 11:54:31 +0530

    Adding a designer for custom activity which should _ideally_ work

---
+++ src/WorkflowContainer/WorkflowContainer.Activities.Design/Metadata.cs
@@ -0,0 +1,26 @@
+﻿using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+using WorkflowContainer.Activities.Design.Properties;
+
+namespace WorkflowContainer.Activities.Design
+{
+    public class Metadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            CategoryAttribute myLocalCategoryAttribute =
+                new CategoryAttribute($"{Strings.CategoryCustom}.{Strings.CategoryMy}");
+
+            AttributeTableBuilder attributeTableBuilder = new AttributeTableBuilder();
+
+            // Register Designers, display name, category etc.
+            //per activity design
+            attributeTableBuilder.AddCustomAttributes(typeof(RequestHumanInput<>), new DesignerAttribute(typeof(RequestHumanInputDesigner)));
+            attributeTableBuilder.AddCustomAttributes(typeof(RequestHumanInput<>), new DisplayNameAttribute(Strings.RequestHumanInputDisplayName));
+            attributeTableBuilder.AddCustomAttributes(typeof(RequestHumanInput<>), myLocalCategoryAttribute);
+
+            // Apply the metadata
+            MetadataStore.AddAttributeTable(attributeTableBuilder.CreateTable());
+        }
+    }
+}
