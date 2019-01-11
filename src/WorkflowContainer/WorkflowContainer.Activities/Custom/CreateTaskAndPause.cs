ef04c0b61324ebcd55ca0ce1dd0875aeae6f160b
chakrabar <chakrabar@live.com>
2019-01-11 15:02:44 +0530

    New workflows

---
+++ src/WorkflowContainer/WorkflowContainer.Activities/Custom/CreateTaskAndPause.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Activities;
+using System.Diagnostics;
+
+namespace WorkflowContainer.Activities.Custom
+{
+    public class CreateTaskAndPause<TOut> : NativeActivity<TOut>
+    {
+        [RequiredArgument]
+        public InArgument<string> BookmarkName { get; set; }
+
+        [RequiredArgument]
+        public InArgument<string> RequestMessage { get; set; }
+
+        [RequiredArgument]
+        public InArgument<string> AsigneeEmail { get; set; }
+
+        public OutArgument<Guid> TaskId { get; set; }
+
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        public void OnResumeBookmark(NativeActivityContext context, Bookmark bookmark, object data)
+        {
+            Trace.TraceInformation($"Resuming : {nameof(WorkflowRestorePoint<TOut>)}, from bookmark : {bookmark.Name}");
+            TOut output = default(TOut);
+            if (data is TOut)
+            {
+                output = (TOut)data;
+            }
+            else
+            {
+                try
+                {
+                    output = JsonUtils.Cast<TOut>(data);
+
+                    Trace.TraceInformation($"From : {nameof(WorkflowRestorePoint<TOut>)}. Bookmark resume data : {output.ToJson()}");
+
+                    // When the Bookmark is resumed, assign its value to the Result argument
+                    Result.Set(context, output);
+                }
+                catch (InvalidCastException e)
+                {
+                    Trace.TraceError($"From : {nameof(WorkflowRestorePoint<TOut>)}. Error casting data. Exception: {e.Message}");
+                    Trace.Flush();
+                    throw;
+                }
+            }
+            Trace.Flush();
+        }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            Trace.TraceInformation($"Executing {nameof(CreateTask)}");
+            var taskId = Guid.NewGuid();
+            TaskId.Set(context, taskId);
+
+            Trace.TraceInformation($"<<<< CREATED TASK >>>> Asignee: {AsigneeEmail.Get(context)}, " +
+                $"RequestMessage: {RequestMessage.Get(context)}. " +
+                $"Task ID: {taskId.ToString()}");
+
+            Trace.TraceInformation($"Activity : {nameof(WorkflowRestorePoint<TOut>)} creating bookmark : {BookmarkName.Get(context)}, " +
+                $"on WorkflowInstanceId: {context.WorkflowInstanceId.ToString()}");
+
+            // Create a Bookmark and wait for it to be resumed.  
+            context.CreateBookmark(BookmarkName.Get(context), new BookmarkCallback(OnResumeBookmark));
+            Trace.Flush();
+        }
+    }
+}
