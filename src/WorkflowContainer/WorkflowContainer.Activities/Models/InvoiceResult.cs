190dd5bbf9a3d955b8b63eb726949cb6f9082a2b
chakrabar <chakrabar@live.com>
2019-01-11 14:00:10 +0530

    The system automatically converts integer from input JSON to Int64! For now, fixing by changing the data types from `int` to `long`.

--- src/WorkflowContainer/WorkflowContainer.Activities/Models/InvoiceResult.cs
+++ src/WorkflowContainer/WorkflowContainer.Activities/Models/InvoiceResult.cs
@@ -2,11 +2,12 @@
 {
     public class InvoiceResult
     {
-        public int Amount { get; set; }
+        public long InvoiceAmount { get; set; }
         public string Level1Approver { get; set; }
         public string Level2Approver { get; set; }
         public ApprovalData Approval1 { get; set; }
         public ApprovalData Approval2 { get; set; }
-        public bool Run { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsAutoApproved { get; set; }
     }
 }
